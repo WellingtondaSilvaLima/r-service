5f28cd179bd61ef66474714751cb321941dc4f5d
Wellington Lima <wellingtonlima.jt@gmail.com>
2025-07-14 12:56:37 -0300

    mongo uri

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import certifi
 
-MONGO_URI = 'mongodb+srv://wellingtonlimaDevPython:Qazujm%401092@nomesteste.ssgjimc.mongodb.net/?retryWrites=true&w=majority&tls=true'
+MONGO_URI = 'mongodb+srv://wellingtonlimaDevPython:Qazujm%401092@nomesteste.ssgjimc.mongodb.net/'
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 
 db = client['nomesTeste']
