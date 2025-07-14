5c8766a45cf161c17f380c9bea60156ffaa8a75d
Wellington Lima <wellingtonlima.jt@gmail.com>
2025-07-14 13:32:10 -0300

    Porta

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import create_app
+import os
 
 def executar():
     app = create_app()
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     executar()
